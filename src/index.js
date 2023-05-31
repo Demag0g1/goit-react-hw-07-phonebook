72cf2b1262d02190efcfb23555c03434ee8f6a89
Demag0g1 <konievanatol@gmail.com>
2023-05-31 17:17:53 +0300

    change package.json

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import './index.css';
 import { store,  } from 'redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+   
         <App />
-      {/* </PersistGate> */}
+ 
     </Provider>
   </React.StrictMode>
 );
